b85817b67d654ce21ee3a57c2be0920e0ecbeb83
weeestar <leonaaardo.b@gmail.com>
2026-04-29 03:09:24 +0200

    fix: properly type Drizzle db singleton so db.query.* is fully typed

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,14 +2,16 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-declare global {
-  // eslint-disable-next-line no-var
-  var __db: ReturnType<typeof drizzle> | undefined
-}
-
 function createDb() {
   const client = postgres(process.env.DATABASE_URL!)
   return drizzle(client, { schema })
+}
+
+type Db = ReturnType<typeof createDb>
+
+declare global {
+  // eslint-disable-next-line no-var
+  var __db: Db | undefined
 }
 
 export const db = globalThis.__db ?? createDb()
