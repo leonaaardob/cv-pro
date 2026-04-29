48550fcde45bff776cf7cb1162c94a1f4be2ab93
weeestar <leonaaardo.b@gmail.com>
2026-04-29 02:23:02 +0200

    fix: use singleton pattern for DB client to prevent connection exhaustion

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -2,5 +2,18 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema'
 
-const client = postgres(process.env.DATABASE_URL!)
-export const db = drizzle(client, { schema })
+declare global {
+  // eslint-disable-next-line no-var
+  var __db: ReturnType<typeof drizzle> | undefined
+}
+
+function createDb() {
+  const client = postgres(process.env.DATABASE_URL!)
+  return drizzle(client, { schema })
+}
+
+export const db = globalThis.__db ?? createDb()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.__db = db
+}
